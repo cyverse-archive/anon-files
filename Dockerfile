087e7043db71a957ffdb316558dad444b299f40a
Ian McEwen <mian@cyverse.org>
2016-09-06 12:24:18 -0700

    Add Jenkinsfile, update various stuff around it

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,24 @@
-FROM discoenv/javabase
+FROM clojure:alpine
 
-USER root
 VOLUME ["/etc/iplant/de"]
 
-COPY conf/main/logback.xml /
-COPY target/anon-files-standalone.jar /
-
 ARG git_commit=unknown
-ARG buildenv_git_commit=unknown
 ARG version=unknown
 LABEL org.iplantc.de.anon-files.git-ref="$git_commit" \
-      org.iplantc.de.anon-files.version="$version" \
-      org.iplantc.de.buildenv.git-ref="$buildenv_git_commit"
+      org.iplantc.de.anon-files.version="$version"
 
-RUN ln -s "/opt/jdk/bin/java" "/bin/anon-files"
+COPY . /usr/src/app
+COPY conf/main/logback.xml /usr/src/app/logback.xml
+
+WORKDIR /usr/src/app
+
+RUN apk add --update git && \
+    rm -rf /var/cache/apk
+
+RUN lein uberjar && \
+    cp target/anon-files-standalone.jar . && \
+    lein clean
+
+RUN ln -s "/usr/bin/java" "/bin/anon-files"
+
 ENTRYPOINT ["anon-files", "-Dlogback.configurationFile=/etc/iplant/de/logging/anon-files-logging.xml", "-cp", ".:anon-files-standalone.jar", "anon_files.core"]
-
--- project.clj
+++ project.clj
@@ -7,21 +7,21 @@
       (string/trim (:out (sh "git" "rev-parse" "HEAD")))
       ""))
 
-(defproject org.iplantc/anon-files "5.2.8.0"
+(defproject org.cyverse/anon-files "2.8.0-SNAPSHOT"
   :description "Serves up files and directories that are shared with the anonymous user in iRODS."
-  :url "https://github.com/iPlantCollaborativeOpenSource/DE"
+  :url "https://github.com/cyverse-de/anon-files"
   :license {:name "BSD"}
   :manifest {"Git-Ref" ~(git-ref)}
   :uberjar-name "anon-files-standalone.jar"
   :main ^:skip-aot anon-files.core
   :profiles {:uberjar {:aot :all}}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.iplantc/clj-jargon "5.2.8.0"
+                 [org.cyverse/clj-jargon "2.8.0"
                   :exclusions [[org.slf4j/slf4j-log4j12]
                                [log4j]]]
-                 [org.iplantc/service-logging "5.2.8.0"]
-                 [org.iplantc/common-cli "5.2.8.0"]
-                 [org.iplantc/common-cfg "5.2.8.0"]
+                 [org.cyverse/service-logging "2.8.0"]
+                 [org.cyverse/common-cli "2.8.0"]
+                 [org.cyverse/common-cfg "2.8.0"]
                  [com.cemerick/url "0.1.1"]
                  [medley "0.6.0"]
                  [compojure "1.5.0"]
