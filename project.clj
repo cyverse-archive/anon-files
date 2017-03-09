9de242779259346fb9a108b8a39603947963abff
Ian McEwen <mian@cyverse.org>
2017-03-08 23:25:36 -0700

    Update clojure-commons version, mostly to test jenkins

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
                                [log4j]]]
                  [org.cyverse/service-logging "2.8.0"]
                  [org.cyverse/common-cli "2.8.0"]
-                 [org.cyverse/clojure-commons "2.8.0"]
+                 [org.cyverse/clojure-commons "2.8.2"]
                  [org.cyverse/event-messages "0.0.1"]
                  [com.novemberain/langohr "3.5.1"]
                  [com.cemerick/url "0.1.1"]
